1bc0ae1d4e1661e5ba8e7a5a3768afb052076803
Zolizoli110 <73071177+Zolizoli110@users.noreply.github.com>
2022-09-20 13:42:54 +0200

    calc

---
+++ main.js
@@ -0,0 +1,7 @@
+function submit(e){
+    let height = parseFloat(document.getElementById('height').value);
+    let weight = parseFloat(document.getElementById('weight').value);
+
+}
+
+document.getElementById('submit').addEventListener('click', submit);
